e30889a1af044260a51260b286c2665113679de4
Yehor <qugar04@gmail.com>
2025-09-06 03:41:29 +0300

    fix

--- components/AuthNavigation/AuthNavigation.tsx
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -29,7 +29,7 @@ export default function AuthNavigation() {
             </Link>
           </li>
           <li className={css.navigationItem}>
-            <p className={css.userEmail}>{user?.username}</p>
+            <p className={css.userEmail}>{user?.email}</p>
             <button className={css.logoutButton} onClick={handleClickLogOut}>
               Logout
             </button>
