9b4f49894d509746b08fdf21bb0ec8bb462b0bb1
Yehor <qugar04@gmail.com>
2025-09-06 04:13:16 +0300

    fix

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -2,6 +2,25 @@ import Link from "next/link";
 import css from "./ProfilePage.module.css";
 import Image from "next/image";
 import { getMe } from "@/lib/api/serverApi";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Your profile",
+  description: "Your personal profile",
+  openGraph: {
+    title: "Your profile",
+    description: "Your personal profile",
+    url: "https://09-auth-self-eight.vercel.app/profile",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Your profile",
+      },
+    ],
+  },
+};
 
 export default async function Profile() {
   const user = await getMe();
--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { checkSession, getMe } from "@/lib/api/clientApi";
 import { useAuthStore } from "@/lib/store/authStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface AuthProviderProps {
   children: React.ReactNode;
 }
 
 export default function AuthProvider({ children }: AuthProviderProps) {
+  const [loading, setLoading] = useState(true);
   const setUser = useAuthStore((state) => state.setUser);
   const clearIsAuthenticated = useAuthStore(
     (state) => state.clearIsAuthenticated
@@ -17,15 +18,22 @@ export default function AuthProvider({ children }: AuthProviderProps) {
       const isAuthenticated = await checkSession();
       if (isAuthenticated) {
         const user = await getMe();
+        setLoading(false);
         if (user) {
           setUser(user);
+          setLoading(false);
         }
       } else {
         clearIsAuthenticated();
+        setLoading(false);
       }
     };
     fetchUser();
   }, [setUser, clearIsAuthenticated]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return children;
 }
